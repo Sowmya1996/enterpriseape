9281a9b85a5408fa089eccf573dafe161f13e621
Sowmya1996 <sowmyapraksh1967@gmail.com>
2019-09-17 12:41:27 +0530

    Integrating alert message

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
-Actionmailer::Base.delivery_method = :smtp
-Actionmailer::Base.smtp_settings = {
-	:address           => 'smtp.sendgrid.net',
-	:port              => '587',
-	:authentication    =>
-	:user_name         =>
-	:password          =>
-	:domain            => 'heroku.com',
-	:enable_starttls_auto => true
-}
+# Actionmailer::Base.delivery_method = :smtp
+# Actionmailer::Base.smtp_settings = {
+# 	:address           => 'smtp.sendgrid.net',
+# 	:port              => '587',
+# 	:authentication    =>
+# 	:user_name         =>
+# 	:password          =>
+# 	:domain            => 'heroku.com',
+# 	:enable_starttls_auto => true
+# }
